4f3b5a15a7fb43605d4a0d007aa5299edac5cfa1
Sandaruwan Chandrasena <schandrasena98@gmail.com>
2024-05-12 16:35:46 +0530

    Update AddTaskActivity.kt file with comments

--- app/src/main/java/com/example/taskmanagementtodoapp/AddTaskActivity.kt
+++ app/src/main/java/com/example/taskmanagementtodoapp/AddTaskActivity.kt
@@ -16,15 +16,24 @@ class AddTaskActivity : AppCompatActivity() {
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Initialize the database helper
         db = TaskDatabaseHelper(this)
 
-
+        // Set up click listener for the save button
         binding.saveButton.setOnClickListener {
             val title = binding.titleEdittext.text.toString()
             val content = binding.contentEditText.text.toString()
+
+            // Create a Note object with the entered title and content
             val note = Task(0,title,content)
+
+            // Insert the note into the database
             db.insertNote(note)
+
+            // Finish the activity (close the AddNoteActivity)
             finish()
+
+            // Show a toast message indicating that the note is saved
             Toast.makeText(this,"Note Saved", Toast.LENGTH_SHORT).show()
         }
     }
